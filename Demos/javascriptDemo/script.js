eb3a516357895c064302e5fb3a7897ecd4832e77
Kristine Aarak <kraar8099@oslomet.no>
2026-02-13 16:12:11 +0100

    Updated code,as the last was an unfinished version

--- Demos/javascriptDemo/script.js
+++ Demos/javascriptDemo/script.js
@@ -1,76 +1,119 @@
+/* */
+/* let and const */
 let age = 20;
 const name = "Kristine";
 
-console.log("hi my name is " + name);
-console.log(`Hi, use my name ${name}`);
 
+/* dynamic */
+let shoppingStatus = "In store";
+shoppingStatus = true;
+
+
+/* Datatypes */
+const customerName = "Arushan";
+const numberOfItems = 10; /* One number type */
+const totalPrice = 99.99;
+const isMember = true;
+let discount; /* Undefined */
+const coupon = null; /* Empty on purpose */
+
+console.log(typeof totalPrice); /* Show difference through typeof's */
+
+console.log("Hi, my name is " + customerName);
+console.log(`Hi, my name is ${customerName}`);
+
+
+/* if/else*/
 const pin = "1234";
 
-if(pin === 1234){
-    console.log("payment accepted")
-}
-else{
-    console.log("wrong pin")
-}
-
-for(let i = 0; i < 5; i++){
-    console.log("Scanning items number ", i)
+if (pin === 1234) { /*show difference of == and === */
+    console.log("Payment approved!");
+} else {
+    console.log("Wrong PIN – payment declined!");
 }
 
-/*create a loop that prints only even numbers from 1-10*/
 
-let shoppingCart = ["Milk", "egg", "beans"];
-shoppingCart.pop();
-shoppingCart.push("wine");
+/*for-loop*/
+for (let i = 0; i < 5; i++) {
+    console.log("Scanning item number:", i);
+} /* why not const*/
 
-shoppingCart.forEach(item =>{
-    console.log(`item: ${item}`)
-})
 
-/*function logItems(item) {
-    console.log(`item: ${item}`)
-}
-shoppingCart.forEach(logItems)
-*/
+/* Student task, 5 min */
+/* Make a for loop that only writes out even numbers from 0-10 (0, 2, 4, 6, 8, 10) */
 
+
+/*Arrays*/
+
+let shoppingCart = ["Milk", "Bread", "Cheese", "Eggs", "Chocolate"];
+
+shoppingCart.forEach(item => { /*arrow function*/
+    console.log("Cart item:", item);
+});
+
+shoppingCart.pop(); /* removes the last one */
+shoppingCart.push("Apples"); /* Adds item to the back */
+
+
+/* Objects */
 const person = {
-    name: "Jorgen",
+    name: "Jørgen",
     age: 24,
     greet: function() {
-        console.log("Hi i am " + this.name + " and I am shopping beans.")
+        console.log("Hi, I am " + this.name + " and I am shopping beans today.");
+        /*Compare to using 'name' as this is a variable further up*/
     }
-}
+};
+
 person.greet();
-person.name ="Jorgen the bean lover";
+person.name="Jørgen the bean lover"; /*How even though the Object is const, it is a reference/points to location in memory. example of bucket.*/
 person.greet();
 
-/*Task: Create a student object with: name, age and course.
-Add a function that logs a sentence about the student.
- */
+
+/* Student task, 5 min */
+/* Make a student object with name, age and course. Make a function that writes out a sentence about the student */
 
 
-/*const message = document.getElementById("message");
-
-
-const userInput = document.getElementById("userInput");
+/* DOM, connecting to html */
+/* Making an input and button in html */
 const btn = document.getElementById("btn");
+const message = document.getElementById("message");
+const userInput = document.getElementById("userInput");
 
 btn.addEventListener("click", () => {
-    message.innerHTML = `Welcome to the store ${userInput.value}`
-})
- */
+    message.innerHTML = "Welcome to the store, " + userInput.value + "!";
+});
+/*Also show onclick in html as alternative*/
 
 
+/*Student task, 5 min */
+/*Make a user input and button, and console.log the input when the button is clicked*/
 
 
+/*Async, await*/
+/*Explain how code is loaded, can take time etc.*/
+console.log("Entering the grocery store");
+
+setTimeout(() => {
+    console.log("Picking up groceries...");
+}, 2000);
+
+console.log("Waiting in line at checkout");
+
+/* Which order will they be written? */
+
+/* What is async code, waiting on answer from server, and don't want the code to run before getting it, or sometimes want it to not stop*/
+
+/*Promises = an object that manages asynchronous operations and promises to return a value*/
 function entering(){
     return new Promise((resolve, reject) => {
-        const success = true;
+        const success = false;
+
         if(success){
-            resolve("Enter the grocery store");
+            resolve("Entering the grocery store")
         }
         else{
-            reject("You didn't go to the grocery store today!")
+            reject("You didn't enter the grocery store!")
         }
     })
 }
@@ -80,12 +123,12 @@ function groceries(){
         setTimeout(() => {
             const success = true;
             if(success){
-                resolve("Get groceries");
+                resolve("Picking up groceries...");
             }
             else{
-                reject("You didn't get any groceries!")
+                reject("You didn't pick up groceries")
             }
-        }, 2000)
+        }, 2000);
     })
 }
 
@@ -93,14 +136,16 @@ function checkout(){
     return new Promise((resolve, reject) => {
         const success = true;
         if(success){
-            resolve("Pay!");
+            resolve("Waiting in line at checkout..");
         }
         else{
-            reject("You thief!")
+            reject("You didn't end up buying anything")
         }
     })
 }
 
+/*One way to call these functions like we want is method chaining, using .then, but what I'll show today is using await
+ */
 async function goShopping(){
     const enteringResult = await entering();
     console.log(enteringResult);
@@ -111,7 +156,42 @@ async function goShopping(){
     const checkoutResult = await checkout();
     console.log(checkoutResult);
 
-    console.log("You can go home now")
+    console.log("You've exited the grocery store!")
 }
 
 goShopping();
+
+
+/*Student task (rest of time)
+Write down these functions and make an async function that uses the functions to write out the complete sentence:
+Hi there
+ */
+
+function word_Jorgen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = true;
+            if(success){
+                resolve("Hi ");
+            }
+            else{
+                reject("Too many beans!")
+            }
+        }, 3000);
+    })
+}
+
+function word_loves(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>
+        {
+            const success = true;
+            if(success){
+                resolve(" there ");
+            }
+            else{
+                reject("Too many beans!")
+            }
+        }, 1000);
+    })
+}
